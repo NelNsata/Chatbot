5bddfc5b90b4521c3f3f1c2525a6e84c22f41ecf
NelNsata <vip1185747@gmail.com>
2023-01-24 11:28:58 +0700

    Add .env Fix index.js loginBot

--- Nbot/index.js
+++ Nbot/index.js
@@ -1,10 +1,12 @@
-const {Client, Intents} = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv');
 
-const client = Client(
-    {
-        Intents:[
-            Intents.FLAGS.GUILDS,
-            Intents.FLAGS.GUILDS_MESSAGES,
-        ]
-    }
-)
+dotenv.config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+
+client.login(process.env.TOKEN)
