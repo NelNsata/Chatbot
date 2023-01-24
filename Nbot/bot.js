8efe24996bf9aed012673181c2330678c1645ada
NelNsata <vip1185747@gmail.com>
2023-01-24 12:48:09 +0700

    test1.1

---
+++ Nbot/bot.js
@@ -0,0 +1,43 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import { ChatGPT } from "discord-chat-gpt";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+  allowedMentions: {
+    repliedUser: false,
+  },
+});
+
+const chatGpt = new ChatGPT({
+  apiKey: `OPEN_AI_KEY`, // get from https://beta.openai.com/account/api-keys
+  orgKey: `Organization_ID`, // get from https://beta.openai.com/account/org-settings
+});
+
+client.on("ready", () => {
+  console.log(`> ${client.user.username} is Online !!`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (!message.guild || message.author.bot) return;
+  let ChatBotChannelId = "ChannelID";
+  let channel = message.guild.channels.cache.get(ChatBotChannelId);
+  if (!channel) return;
+  if (message.channel.id === channel.id) {
+    let msg = await message.reply({
+      content: `Waiting ....`,
+    });
+    let chatreply = await chatGpt
+      .chat(message.content, message.author.username)
+      .catch((e) => {
+        console.log(e);
+      });
+    msg.edit({
+      content: `${chatreply}`,
+    });
+  }
+});
--- Nbot/index.js
+++ Nbot/index.js
@@ -1,18 +1,60 @@
-const { Client, GatewayIntentBits, MessageManager, MessageCollector, Message } = require('discord.js');
-const dotenv = require('dotenv');
+// const { Client, GatewayIntentBits, MessageManager, MessageCollector, Message } = require('discord.js');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-  });
+// client.on('ready', () => {
+//     console.log(`Logged in as ${client.user.tag}!`);
+//   });
 
-client.on('messageCreate', message=>{
-    console.log(message.content == 'hungry'){
-        message.reply('order')
-    }
-});
+// client.on('messageCreate', message=>{
+//     if (message.content == 'hungry'){
+//         message.reply('order')
+//     }
+// });
 
-client.login(process.env.TOKEN)
+// client.login(process.env.TOKEN)
+
+import { Configuration, OpenAIApi } from "openai";
+
+class ChatGPT {
+  /**
+   *
+   * @param {import("./types").ChatGPTOptions} options
+   */
+  constructor(options) {
+    this.orgKey = options.authId;
+    this.apiKey = options.apiKey;
+  }
+
+  async chat(content, requester) {
+    const configuration = new Configuration({
+      organization: this.orgKey,
+      apiKey: this.apiKey,
+    });
+    const openai = new OpenAIApi(configuration);
+    let response = await openai
+      .createCompletion({
+        model: "davinci",
+        prompt: `ChatGPT is a friendly chatbot. \n\
+        ChatGPT : Hello, how are you?\n\
+        ${requester}: ${content}\n\
+        ChatGPT:`,
+        temperature: 0.9,
+        max_tokens: 100,
+        stop: ["ChatGPT:", "Kabir Jaipal:"],
+      })
+      .then((res) => {
+        return res.data.choices.at(0).text;
+      })
+      .catch((e) => {
+        return e.message;
+      });
+
+    return response;
+  }
+}
+
+export { ChatGPT };
